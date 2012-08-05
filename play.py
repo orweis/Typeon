9fc6315c18d453ec25c48035ec07bc5ed5698787
Or Weis <orweis@gmail.com>
2012-08-05 09:09:20 +0300

    Add some temp work files  (These are not meant to be used by users)

---
+++ play.py
@@ -0,0 +1,6 @@
+import scanner
+import imp
+
+s = scanner.TypeonTyper(False)
+m = s.handleModuleAtPath(r"C:\Proj\typeon\test2.py")
+#c = compile(m, "test2", "exec")
---
+++ test2.py
@@ -0,0 +1,32 @@
+import typeon
+T = None
+
+class Z:
+    def __init__(self):
+        self.data = "GGG"
+
+#New style types
+##def doMath( r = int(6), fraction=float() , z="KKK" ):
+
+
+def doMath( r = T:int(6), fraction=float() , z="KKK" ):
+    pass
+
+
+
+    
+def doMath( (int,r), (float,fraction) , z="KKK" ):
+    if item is True:
+        q = (jump(y) + h)
+    r = r + 1
+    r = r + ""
+    return r / int(fraction)
+
+
+def mainTest():
+    doMath("gg", "88")
+    doMath(1, "h")
+    
+
+
+typeon.compile()
